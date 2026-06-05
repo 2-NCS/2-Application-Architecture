2dc079573e94aaae97f662802bb0b55274062d76
kanghyunuk <hukang.dev@gmail.com>
2026-06-05 14:52:32 +0900

    Commons-DBCP2 기반의 dataSource2 빈 생성 및 MySQL 연결 설정

--- EX01/src/main/java/com/example/demo/Config/DataSourceConfig.java
+++ EX01/src/main/java/com/example/demo/Config/DataSourceConfig.java
@@ -1,6 +1,10 @@
 package com.example.demo.Config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // TODO: DataSource 빈 2개를 작성하라.
 //  필요한 import : javax.sql.DataSource, org.springframework.context.annotation.Bean,
@@ -11,9 +15,19 @@ public class DataSourceConfig {
     // TODO 1) @Bean dataSource2() : BasicDataSource(commons-dbcp2)
     //        - setDriverClassName("com.mysql.cj.jdbc.Driver")
     //        - setUrl("jdbc:mysql://localhost:3306/testdb"), setUsername("root"), setPassword("1234")
+    @Bean
+    public DataSource dataSource2() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/testdb");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+        return dataSource;
+    }
     // TODO 2) @Bean dataSource3() : HikariDataSource
     //        - setDriverClassName("com.mysql.cj.jdbc.Driver")
     //        - setJdbcUrl("jdbc:mysql://localhost:3306/testdb"), setUsername("root"), setPassword("1234")
     //  ※ MemberDAO 는 dataSource3(HikariDataSource) 를 주입받아 사용한다.
+
 
 }
