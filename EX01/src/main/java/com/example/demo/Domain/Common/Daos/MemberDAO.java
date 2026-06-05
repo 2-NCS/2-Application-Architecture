b803b833acdddc9e3c0af9fe279b9f845fcf40d7
kinetas <kimjh7302@naver.com>
2026-06-05 14:33:16 +0900

    ResultSet 을 돌며 MemberDTO.builder() 로 매핑하여 List 로 반환

--- EX01/src/main/java/com/example/demo/Domain/Common/Daos/MemberDAO.java
+++ EX01/src/main/java/com/example/demo/Domain/Common/Daos/MemberDAO.java
@@ -5,11 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -40,6 +38,20 @@ public class MemberDAO {
     public List<MemberDTO> selectAll() throws SQLException {
         Connection conn = dataSource3.getConnection();
         PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM tbl_member ORDER BY id DESC ");
+        ResultSet rs = pstmt.executeQuery();
+        List<MemberDTO> list = new ArrayList<>();
+        MemberDTO dto = null;
+        while (rs.next()){
+            dto = MemberDTO.builder()
+                    .id(rs.getLong("id"))
+                    .name(rs.getString("name"))
+                    .email(rs.getString("email"))
+                    .phone(rs.getString("phone"))
+                    .createAt(rs.getTimestamp("createAt").toLocalDateTime())
+                    .build();
+            list.add(dto);
+        }
+        return list;
     }
 
 }
